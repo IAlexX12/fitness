6a949127ae9081db44b4ac79ab67c7cef89c4bdf
AP LB <alejandroluque_pabloborrel@outlook.com>
2025-06-30 16:27:42 +0200

    cargacsv

--- cargaCSV.js
+++ cargaCSV.js
@@ -1,13 +1,10 @@
 window.addEventListener('DOMContentLoaded', async function () {
-    // Solo cargar si no está ya en localStorage
-    if (!localStorage.getItem('clasificadosCSV')) {
-        try {
-            const response = await fetch('Data/alimentos.csv');
-            if (!response.ok) throw new Error('No se pudo cargar alimentos.csv');
-            const text = await response.text();
-            localStorage.setItem('clasificadosCSV', text);
-        } catch (err) {
-            alert('Error cargando alimentos.csv: ' + err.message);
-        }
+    try {
+        const response = await fetch('Data/alimentos.csv');
+        if (!response.ok) throw new Error('No se pudo cargar alimentos.csv');
+        const text = await response.text();
+        localStorage.setItem('clasificadosCSV', text);
+    } catch (err) {
+        alert('Error cargando alimentos.csv: ' + err.message);
     }
 });
